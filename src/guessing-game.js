5a8f491ac7e3dd7db50ebfcd9d0e47b22a74164d
Tatsiana Zare <danee4ka2@mail.ru>
2017-01-31 23:11:16 +0300

    Implement all functions

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.half = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
+    	this.half = this.findHalf(this.min, this.max);
 
+    	return this.half;
     }
 
     lower() {
-
+    	this.max = this.half;
     }
 
     greater() {
+    	this.min = this.half;
+    }
 
+    findHalf(min, max) {
+
+    	return Math.round((min + max) / 2);
     }
 }
 
